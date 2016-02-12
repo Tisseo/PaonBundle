2cbc8fe999289cbf73427c2ee7a3287bd6cdc729
Jean Lavat <jean.lavat@capgemini.com>
2016-02-12 15:04:23 +0100

    editing controller, views and forms for adding lgg printings export feature

--- Controller/LineGroupGisController.php
+++ Controller/LineGroupGisController.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Tisseo\EndivBundle\Entity\Line;
 use Tisseo\EndivBundle\Entity\LineGroupGis;
 use Tisseo\EndivBundle\Entity\LineGroupGisContent;
+use Tisseo\EndivBundle\Entity\PrintingLineGroupGis;
 use Tisseo\PaonBundle\Form\Type\LineGroupGisType;
+use Tisseo\PaonBundle\Form\Type\PrintingLineGroupGisType;
 use Tisseo\CoreBundle\Controller\CoreController;
 
 class LineGroupGisController extends CoreController
@@ -140,4 +142,66 @@ class LineGroupGisController extends CoreController
 
         return $response;
     }
+
+    /**
+     * Create printingLineGroupGis
+     * @param integer $lineGroupGisId
+     *
+     * Creating PrintingLineGroupGis
+     */
+    public function addPrintingsAction(Request $request, $lineGroupGisId)
+    {
+        $this->isGranted('BUSINESS_MANAGE_GROUP_GIS');
+
+        $lineGroupGisManager = $this->get('tisseo_endiv.line_group_gis_manager');
+        $lineGroupGis = $lineGroupGisManager->find($lineGroupGisId);
+
+        if (empty($lineGroupGis))
+        {
+            $this->addFlash('warning', 'tisseo.paon.line_group_gis.message.not_found');
+            return $this->redirectToRoute('tisseo_paon_line_group_gis_list');
+        }
+
+        $printingLineGroupGis = new PrintingLineGroupGis();
+        $printingLineGroupGis->setLineGroupGis($lineGroupGis);
+
+        $form = $this->createForm(
+            new PrintingLineGroupGisType(),
+            $printingLineGroupGis,
+            array(
+                'action' => $this->generateUrl(
+                    'tisseo_paon_line_group_gis_printing_create',
+                    array(
+                        'lineGroupGisId' => $lineGroupGis->getId()
+                    )
+                ),
+                'em' => $this->get('doctrine.orm.endiv_entity_manager')
+            )
+        );
+
+        $form->handleRequest($request);
+        if ($form->isValid())
+        {
+            try
+            {
+                $this->get('tisseo_endiv.printing_line_group_gis_manager')->save($form->getData());
+                $this->addFlash('success', 'tisseo.flash.success.created');
+            }
+            catch (\Exception $e)
+            {
+                $this->addFlashException($e->getMessage());
+            }
+
+            return $this->redirectToRoute('tisseo_paon_line_group_gis_list');
+        }
+
+        return $this->render(
+            'TisseoPaonBundle:LineGroupGis:printing.html.twig',
+            array(
+                'title' => 'tisseo.paon.line_group_gis.printing.title',
+                'form' => $form->createView()
+            )
+        );
+    }
+
 }
---
+++ Form/Type/PrintingLineGroupGisType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tisseo\PaonBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+use Tisseo\EndivBundle\Entity\LineGroupGis;
+use Tisseo\CoreBundle\Form\DataTransformer\EntityToIntTransformer;
+
+class PrintingLineGroupGisType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $entityTransformer = new EntityToIntTransformer($options["em"]);
+        $entityTransformer->setEntityClass("Tisseo\\EndivBundle\\Entity\\LineGroupGis");
+        $entityTransformer->setEntityRepository("TisseoEndivBundle:LineGroupGis");
+        $entityTransformer->setEntityType("lineGroupGis");
+
+        $builder
+            ->add(
+                'quantity',
+                'number',
+                array(
+                    'label' => 'tisseo.paon.printing.label.quantity',
+                    'precision' => 0
+                )
+            )
+            ->add(
+                'date',
+                'tisseo_datepicker',
+                array(
+                    'label' => 'tisseo.paon.printing.label.date',
+                    'attr' => array(
+                        'data-from-date' => true
+                    )
+                )
+            )
+            ->add(
+                'comment',
+                'textarea',
+                array(
+                    'label' => 'tisseo.paon.printing.label.comment',
+                    'required' => false
+                )
+            )
+            ->add(
+                $builder->create(
+                    'lineGroupGis',
+                    'hidden'
+                )->addModelTransformer($entityTransformer)
+            )
+            ->setAction($options['action'])
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'data_class' => 'Tisseo\EndivBundle\Entity\PrintingLineGroupGis'
+            ))
+            ->setRequired(array(
+                'em',
+            ))
+            ->setAllowedTypes(array(
+                'em' => 'Doctrine\Common\Persistence\ObjectManager',
+            ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'paon_printing_line_group_gis';
+    }
+}
